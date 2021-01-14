a5cdb650478b79a0662ac7200bb987fb919f09fc
Ricardo Pérez <ricpelo@gmail.com>
2021-01-14 21:20:22 +0100

    Añade un Yii::debug()

--- controllers/LibrosController.php
+++ controllers/LibrosController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\LibrosForm;
+use Yii;
 use yii\web\Controller;
 
 class LibrosController extends Controller
@@ -10,6 +11,8 @@ class LibrosController extends Controller
     public function actionCreate()
     {
         $librosForm = new LibrosForm();
+
+        Yii::debug(Yii::$app->request->post());
 
         return $this->render('create', [
             'librosForm' => $librosForm,
